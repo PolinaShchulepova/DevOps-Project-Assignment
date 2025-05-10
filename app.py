87d9ac1ffd1bd4693547c3fce38b4fc3d7536c32
Outmezgine <d.outmezgine@gmail.com>
2025-05-10 19:56:01 +0300

    copied app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import redis
+import random
+
+app = Flask(__name__)
+r = redis.Redis(host=os.getenv('REDIS_HOST', 'localhost'),port=6379, decode_responses=True)
+
+
+@app.route('/quote')
+def get_quote():
+    quotes = r.lrange("quotes", 0, -1) 
+    if not quotes:
+        return "No quotes available yet"
+    return random.choice(quotes)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
