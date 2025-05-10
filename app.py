8cbf0dac49c49c92153eec947006950ece11b74f
Polina <poinkash28@gmail.com>
2025-05-10 18:35:06 +0300

    Added initial code for returning a random quote and a requirements file.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import redis
+import random
+
+app = Flask(__name__)
+r = redis.Redis(host='redis', port=6379, decode_responses=True)
+
+@app.route('/quote')
+def get_quote():
+    quotes = r.lrange("quotes", 0, -1) 
+    if not quotes:
+        return "No quotes available yet"
+    return random.choice(quotes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
